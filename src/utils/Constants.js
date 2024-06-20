1f3990dd1e7f9ed9ec61a706e22c8763498f84b5
Ayaan Kapoor <96294988+kapoorayaan@users.noreply.github.com>
2024-06-20 13:20:36 +0530

    Update Constants.js

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,6 +1,6 @@
 import { useSearchParams } from "react-router-dom";
 
-export const gApiKey = "AIzaSyA9AyIgqSYfJMNOdq5vM-q3Flrzi-zeFX0";
+export const gApiKey = "your API;
 
 export const ytApi =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
